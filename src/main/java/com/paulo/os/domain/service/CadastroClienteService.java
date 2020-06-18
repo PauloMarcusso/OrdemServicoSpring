b39361270d6e8312365ee14975a9b440cdfe945d
Paulo H <phmarcusso1@hotmail.com>
2020-06-18 07:46:27 -0300

    CRUD, validacao e tratamento de excecoes

---
+++ src/main/java/com/paulo/os/domain/service/CadastroClienteService.java
@@ -0,0 +1,30 @@
+package com.paulo.os.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.paulo.os.api.domain.model.Cliente;
+import com.paulo.os.domain.exception.NegocioException;
+import com.paulo.os.domain.repository.ClienteRepository;
+
+@Service
+public class CadastroClienteService {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	public Cliente salvar(Cliente cliente) {
+
+		Cliente clienteExistente = clienteRepository.findByEmail(cliente.getEmail());
+
+		if (clienteExistente != null && !clienteExistente.equals(cliente)) {
+			throw new NegocioException("Já existe um cliente cadastrado com este e-mail");
+		}
+
+		return clienteRepository.save(cliente);
+	}
+
+	public void excluir(Long clienteId) {
+		clienteRepository.deleteById(clienteId);
+	}
+}
